97ee09427a8877c8c1d629417b32808065c05793
Abdul Mannan <fahadfateh1000@gmail.com>
2025-05-21 18:52:04 +0430

    set the app layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\CarController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
