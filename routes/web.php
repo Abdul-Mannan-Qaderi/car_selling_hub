ae1e32ef6c56597993e9f250f7b5b2646638b49d
Abdul Mannan <fahadfateh1000@gmail.com>
2025-05-22 15:12:00 +0430

    create login page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\CarController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/signup', [SignupController::class, 'create']);
+Route::get('/login', [LoginController::class, 'login']);
