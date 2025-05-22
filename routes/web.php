f0a954da8bad097c9a7c081fe4a5f3b686c9f415
Abdul Mannan <fahadfateh1000@gmail.com>
2025-05-22 15:00:59 +0430

    create signup page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\CarController;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/signup', [SignupController::class, 'create']);
